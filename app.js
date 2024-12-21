9bf9513f6fd813ac620a18e1bcfe7698f224c2a5
sufyaanmalik <sufyaanmaalik@gmail.com>
2024-12-22 01:33:46 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+// Chapter 5
+
+// Question # 1 And # 2
+
+document.write("<h3>Answer 1 & 2</h3>");
+
+
+let a = 3;
+let b = 5;
+let total = a + b;
+
+let min = a - b;
+let multi = a * b;
+let divi = a / b;
+
+document.write("Sum of " + a + " And " + b + " is " + total + "<br>");
+document.write("Subtraction of " + a + " And " + b + " is " + min + "<br>");
+document.write("Multiplication of " + a + " And " + b + " is " + multi + "<br>");
+document.write("Division of " + a + " And " + b + " is " + divi + "<br><br>");
+
+document.write("<h3>Answer 3</h3>");
+
+
+// Question # 3
+
+let x = 5;
+document.write("Intial Value is " + x + "<br><br>");
+x++;
+document.write("Value after increament is " + x + "<br><br>");
+let z = 7;
+let addi = x + z;
+document.write("Value after Addition is " + addi + "<br><br>");
+--addi;
+document.write("Value after decrement is" + addi + "<br><br>");
+
+
+document.write("<h3>Answer 4</h3>");
+
+
+// Question # 4
+
+let ticket_price = 600;
+document.write("Total cost to buy 5 tickets is " + ticket_price * 5 + "<br><br>");
+
+
+document.write("<h3>Answer 5</h3>");
+
+// Question # 5
+
+let i = 4;
+document.write("Table of 4 <br><br>");
+document.write(i + " x " + 1 + " = " + i * 1 + "<br>");
+document.write(i + " x " + 2 + " = " + i * 2 + "<br>");
+document.write(i + " x " + 3 + " = " + i * 3 + "<br>");
+document.write(i + " x " + 4 + " = " + i * 4 + "<br>");
+document.write(i + " x " + 5 + " = " + i * 5 + "<br>");
+document.write(i + " x " + 6 + " = " + i * 6 + "<br>");
+document.write(i + " x " + 7 + " = " + i * 7 + "<br>");
+document.write(i + " x " + 8 + " = " + i * 8 + "<br>");
+document.write(i + " x " + 9 + " = " + i * 9 + "<br>");
+document.write(i + " x " + 10 + " = " + i * 10 + "<br>");
+
+document.write("<h3>Answer 8</h3>");
+
+
+// Question # 8
+
+let total_marks = 100;
+let marks_obtained = 90;
+let percentage = marks_obtained * 100 / total_marks;
+
+document.write("Total Marks is" + total_marks + "<br>");
+document.write("Marks Obtained is" + marks_obtained + "<br><br>");
+document.write("Percentage is " + percentage + "<br><br>");
+
+document.write("<h3>Answer 9</h3>");
+
+// Question # 9
+
+let dolorcr = 104.80;
+let saudicr = 28;
+
+let pkrcr = (dolorcr * 10 ) + (saudicr * 25);
+
+document.write("Total Pkr Currency is " + pkrcr);
